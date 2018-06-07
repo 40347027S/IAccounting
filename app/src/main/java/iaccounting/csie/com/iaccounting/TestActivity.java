a644d5d43f949a2e245b5267496154b6791c51cd
Tail <tail9951@gmail.com>
2018-06-07 21:35:14 +0800

    Drawer可以用繼承的來完成了 切換畫面可以用同一個函式來完成了

--- app/src/main/java/iaccounting/csie/com/iaccounting/TestActivity.java
+++ app/src/main/java/iaccounting/csie/com/iaccounting/TestActivity.java
@@ -13,12 +13,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Drawer{
+public class TestActivity extends Drawer{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
